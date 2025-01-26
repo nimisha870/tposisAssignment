fe75ceb676279ee293e6f676956dd0bd5543bf0c
nimisha870 <124371612+nimisha870@users.noreply.github.com>
2025-01-27 00:17:36 +0530

    Added the python package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Topsis_Nimisha_Gujral_102203894",
+    version="1.0.14",
+    author="Nimisha Gujral",
+    author_email="ngujral_be22@thapar.com",
+    url="https://github.com/nimisha870/tposisAssignment",
+    description="A python package for implementing topsis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pandas", "numpy"],
+    entry_points={"console_scripts": ["Topsis_Nimisha_Gujral_102203894 = Topsis_Nimisha_Gujral_102203894.topsis:main"]},
+)
